6a2802624cf42be585a38e4c885e3e5a1140586e
Joshua Alley <jauschalley@gmail.com>
2019-03-08 16:13:18 -0500

    initial react router

--- app/javascript/components/BikeBinApp.jsx
+++ app/javascript/components/BikeBinApp.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import BikeBinHome from './BikeBinHome'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default class BikeBinApp extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>BikeBinApp component</h1>
-        <BikeBinHome />
-      </div>
-    );
-  }
-}
+
+// export default class BikeBinApp extends React.Component {
+//   render() {
+//     return (
+//       <BikeBinHome />
+//     );
+//   }
+// }
+
+const BikeBinApp = () => {
+  return (
+    <Router>
+      <Route path="/" component={BikeBinHome}>
+      </Route>
+    </Router>
+  );
+};
+
+export default BikeBinApp 
