0b8c295a3d3b08c00141174bfe167205a3124de2
Joshua Alley <jauschalley@gmail.com>
2019-03-07 21:49:05 -0500

    removed leftover comments

--- app/javascript/components/BikeBinApp.jsx
+++ app/javascript/components/BikeBinApp.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import BikeBinHome from './BikeBinHome'
 
 export default class BikeBinApp extends React.Component {
-  // constructor(props, _railsContext) {
-  //   super(props);
-  // }
   render() {
     return (
       <BikeBinHome />
